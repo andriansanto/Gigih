67d5f5658d61c2b8415ef4a9f5ab9994d440a80d
andriansanto <andriansanto.mb1@gmail.com>
2021-07-22 22:00:32 +0700

    exercise module 3 session 2

--- Weekly_Exercise/intermediateclass/src/search/index_search.js
+++ Weekly_Exercise/intermediateclass/src/search/index_search.js
@@ -1,39 +1,33 @@
-import React, { Component } from "react";
+import React, { useEffect,useState } from "react";
 import Gif from "../gif/gif";
 
 const api= process.env.REACT_APP_GIPHY_KEY;
 
 console.log(process.env.REACT_APP_GIPHY_KEY);
     
-class Search extends Component{
-    state = {
-        search: "",
-        data: [],
-    };
-    
-    handleSearch = (e) => {
-        this.setState({ search: e.target.value });
-    }
+function Search(){
 
-    getSearch = () => {
-        fetch(
-          `https://api.giphy.com/v1/gifs/search?api_key=${api}&q=${this.state.search}&limit=6`
+    const [result, setResult] = useState([]);
+    const [search, setSearch] = useState("");
+    
+    useEffect(() => {
+        getSearch(search);
+    })
+    
+
+    const getSearch = async () => {
+        const response = await fetch(
+          `https://api.giphy.com/v1/gifs/search?api_key=${api}&q=${search}&limit=6`
         )
-        .then((response) => response.json())
-        .then((response) => {
-            this.setState({ data: response.data });
-        });
+        .then((response) => response.json());
+        setResult(response.data);
     };
-    
-
-
-    render(){
         return(
             <>
             <div>
-                <input type="text" onChange={(e) => {this.handleSearch(e)}}/>
-                <button onClick={this.getSearch}>Search</button>
-                {this.state.data?.map((gif, i) => {
+                <input value={search} type="text" onChange={(e) => {setSearch(e.target.value)}}/>
+                <button onClick={() => {getSearch()}}>Search</button>
+                {result.map((gif, i) => {
                     return (
                     <Gif key={i} title={gif.title} url={gif.images.original.url}/>
                     );
@@ -42,7 +36,7 @@ class Search extends Component{
             </div>
             </>
         );
-    }
+
 }
 
 export default Search;
